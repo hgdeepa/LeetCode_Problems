f225543a0351a0ec983538b635ac6ecbe497a422
hgdeepa <76605001+hgdeepa@users.noreply.github.com>
2025-02-06 12:13:59 -0600

    [1] [Time Beats: 100.00%] [Memory Beats: 44.06%] - LeetPush

--- 1-Two-Sum.py
+++ 1-Two-Sum.py
@@ -7,13 +7,24 @@ class Solution:
         #             if nums[i]+nums[j+1] == target:
         #                 return [i, j+1]
         #                 break
+   
+        # #using value-x method
+        # nums_size = len(nums)
+        # for i in range (nums_size):
+        #     comp_value = nums[0]
+        #     nums.remove(nums[0])
+        #     if (target - comp_value) in nums:
+        #          for j in range(len(nums)):
+        #             if comp_value+nums[j] == target:
+        #                 return [i, (i+j+1)]
+        #                 break
 
-        nums_size = len(nums)
-        for i in range (nums_size):
-            comp_value = nums[0]
-            nums.remove(nums[0])
-            if (target - comp_value) in nums:
-                 for j in range(len(nums)):
-                    if comp_value+nums[j] == target:
-                        return [i, (i+j+1)]
-                        break
+
+        #hasmap method
+        hashmaptable={}
+        for i, n in enumerate(nums): 
+            diff = target-n 
+            if diff in hashmaptable:
+                return (hashmaptable[diff],i)
+            else:
+                hashmaptable[n]=i
